f269a6a10b86e2d8411e3b7ede68a8c584b79d74
einsJannis <einsjannis@gmail.com>
2022-01-25 08:29:45 +0100

    added parsable trait as an alternative to pattern

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -3,5 +3,14 @@ use crate::WithContentLocation;
 
 pub trait Pattern {
     type Output: WithContentLocation;
-    fn match_pattern(&self, tokens: TokenIterator) -> Self::Output;
+    fn match_pattern(tokens: TokenIterator) -> Self::Output;
+}
+
+pub trait Parsable: WithContentLocation {
+    fn parse(tokens: TokenIterator) -> Self;
+}
+
+impl<T> Pattern for T where T: Parsable {
+    type Output = Self;
+    fn match_pattern(tokens: TokenIterator) -> Self::Output { <Self as Parsable>::parse(tokens) }
 }
